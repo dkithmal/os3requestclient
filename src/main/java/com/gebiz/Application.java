830f3fd2d0e0fb17438f8a95ae0210d6b6266d9e
damitha kithmal <dkithmal@yahoo.com>
2020-04-10 17:36:07 +0800

    updated

--- src/main/java/com/gebiz/Application.java
+++ src/main/java/com/gebiz/Application.java
@@ -2,6 +2,7 @@ package com.gebiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
@@ -12,6 +13,11 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 
 }
